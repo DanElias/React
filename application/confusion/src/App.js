0f07e1aeaeaf08885e5965574b8dd746b817ee5c
Dan <A01208905@itesm.mx>
2020-07-01 14:13:06 -0500

    [DAN] Learn components + props + lifecycle

--- application/confusion/src/App.js
+++ application/confusion/src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Ristorante Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+    
 }
 
 export default App;
